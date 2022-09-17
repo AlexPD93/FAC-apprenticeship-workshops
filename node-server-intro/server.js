e1cb4c6e5ea57e30c6ae12cb7d89076735689efe
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-17 16:26:55 +0100

    Added node-server-intro

---
+++ node-server-intro/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (request, response) => {
+  response.send("hello");
+});
+
+server.get("/uh-oh", (request, response) => {
+  response.status(500).send("something went wrong");
+});
+
+module.exports = server;

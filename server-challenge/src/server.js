e7b0ddb16e86f3b2aa1d1ebb8e2de0f001988c57
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-19 15:38:51 +0100

    Completed task 5

--- server-challenge/src/server.js
+++ server-challenge/src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+const bodyParser = express.urlencoded();
+
 //Challenge 1
 server.get("/", (request, response) => {
   response.send(`
@@ -38,16 +40,32 @@ server.get("/colour", (request, response) => {
   response.send(html);
 });
 
+const cheeses = [];
+
 server.get("/cheese", (request, response) => {
+  const list = cheeses.map((cheese) => {
+    return `<li> ${cheese.name} | ${cheese.rating}</li>`;
+  });
   const html = `
     <form action="/cheese" method="POST">
     <label for="cheeseName">Cheese name</label>
     <input name="cheeseName" >
     <label for="rating">Rating</label>
     <input type="range" name="rating" main="0" max="5">
+    <button>Rate Cheese</button>
     </form>
+    <ul>
+    ${list.join(" ")}
+    </ul>
     `;
   response.send(html);
+});
+
+server.post("/cheese", bodyParser, (request, response) => {
+  const name = request.body.cheeseName;
+  const rating = request.body.rating;
+  cheeses.push({ name, rating });
+  response.redirect("/cheese");
 });
 
 module.exports = server;

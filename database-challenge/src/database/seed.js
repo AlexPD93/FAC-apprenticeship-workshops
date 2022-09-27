a0d24b81731eb0177d0e58660b3b0a3232d295f4
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-27 11:38:25 +0100

    Completed workshop

--- database-challenge/src/database/seed.js
+++ database-challenge/src/database/seed.js
@@ -1,5 +1,6 @@
 const { readFileSync } = require("node:fs");
 const { join } = require("node:path");
+const { listProducts } = require("../model/products.js");
 const db = require("./db.js");
 
 const seedPath = join("src", "database", "seed.sql");
@@ -7,3 +8,4 @@ const seed = readFileSync(seedPath, "utf-8");
 db.exec(seed);
 
 console.log("DB seeded with example data");
+console.log(listProducts());
--- database-challenge/src/model/products.js
+++ database-challenge/src/model/products.js
@@ -1,1 +1,50 @@
 const db = require("../database/db.js");
+
+//Challenge 1
+const select_products = db.prepare(/*sql*/ `
+  SELECT
+    id,
+    name,
+    quantity_per_unit,
+    FORMAT('£%.2f', unit_price) AS unit_price,
+    units_in_stock,
+    units_on_order,
+    FORMAT('£%.2f', unit_price  * units_in_stock) AS stock_value 
+  FROM products
+`);
+
+function listProducts() {
+  return select_products.all();
+}
+
+//Challenge 2
+const search_products = db.prepare(/*sql*/ `
+SELECT 
+id,
+name
+FROM products
+WHERE name LIKE ?
+`);
+
+function searchProducts(searchedProduct) {
+  return search_products.all(`%${searchedProduct}%`);
+}
+
+//Challenge 3 & 4
+
+const specific_product = db.prepare(/*sql*/ `
+SELECT 
+products.id,
+products.name,
+categories.name AS category_name,
+categories.description AS category_description
+FROM products
+JOIN categories ON products.category_id = categories.id 
+WHERE products.id = ?
+`);
+
+function getProduct(id) {
+  return specific_product.get(id);
+}
+
+module.exports = { listProducts, searchProducts, getProduct };
